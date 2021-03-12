66afeaf7203dd82ff34a4987f9e6616d37358a2e
StevePro7 <steveprozune@hotmail.com>
2021-03-12 09:47:09 +0000

    no message

--- asm/05/build.bat
+++ asm/05/build.bat
@@ -18,3 +18,5 @@ if exist main.o del main.o
 echo Run
 ::java -jar C:\SEGA\Emulicious\Emulicious.jar output.sms
 ::output.sms
+
+copy output.sms E:\GitHub\StevePro7\Z80test\05-Utils\BinaryFileXCompare\bin\Debug\input
